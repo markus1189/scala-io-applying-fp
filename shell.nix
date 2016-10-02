b4abce93339c8aee60938e3a38b35529835417ca
Markus Hauck <markus1189@gmail.com>
2016-10-02 16:13:53 +0200

    Nix file

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }: with pkgs;
+
+let
+  hsPkgs = ps: with ps; [
+    containers
+    aeson
+  ];
+in
+stdenv.mkDerivation {
+  name = "applying-fp-patterns";
+  buildInputs = [
+    fasd
+    (texlive.combine {
+        inherit (texlive)
+        scheme-medium
+        beamer
+        listings
+        minted
+        cleveref
+        microtype
+        babel
+        todonotes
+        chngcntr
+        excludeonly
+        upquote
+        ifplatform
+        xstring
+        enumitem;
+      })
+      zathura
+  ] ++ (with haskellPackages; [
+    (ghcWithPackages hsPkgs)
+    shake
+  ]);
+}
