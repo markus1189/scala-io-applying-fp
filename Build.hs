80e4fe70c1ca04b2afae73cfbe5b8f7d35cd03b3
Markus Hauck <markus1189@gmail.com>
2016-10-03 10:03:10 +0200

    Refactor shake build

--- Build.hs
+++ Build.hs
@@ -7,18 +7,21 @@ import Development.Shake.FilePath
 import Development.Shake.Util
 import qualified Data.Text as T
 
+withCwd :: MonadIO m => Turtle.FilePath -> m a -> m a
+withCwd dir act = do
+  cwd <- pwd
+  cd dir
+  r <- act
+  cd cwd
+  return r
+
 main :: IO ()
 main = shakeArgs shakeOptions $ do
     want [ "tex" </> "applying-fp.pdf" ]
 
     "//*.pdf" %> \out -> do
       let inp = out -<.> "tex"
+          dir = fromString (takeDirectory inp)
       need [inp]
-      void $ do
-        let dir = fromString (takeDirectory inp) :: Turtle.FilePath
-        echo (T.pack $ takeDirectory inp)
-        cd dir
-        dir <- pwd
-        echo (format fp dir)
+      void . withCwd dir $ do
         shell ("latexmk -pdf " <> T.pack (takeFileName inp)) empty
-        cd ".."
